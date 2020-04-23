2c07e378238fad939b51e34e83ae7831dadf91b8
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-23 23:00:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,7 @@ class Shoe
  
 
   BRANDS = []
+<<<<<<< HEAD
   
  def initialize(brand)
     @brand = brand
@@ -12,12 +13,27 @@ class Shoe
     else
       BRANDS << brand
     end
+=======
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand
+>>>>>>> 5099de976c5a242d5d505239482ec1a26a3d417c
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+<<<<<<< HEAD
   
+=======
+  def brand=(brand)
+    @brand = brand
+    Shoe:..each do |brands|
+      if brands.!include?(brand)
+      BRANDS << brand
+    end
+  end
+>>>>>>> 5099de976c5a242d5d505239482ec1a26a3d417c
 
 end
